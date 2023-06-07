65c32231a93f632ef63e4106a8ccb9fe2d7aeff6
Kgomotso Nacane <kgomotsonacane@gmail.com>
2023-06-07 14:21:27 +0200

    Add es6

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
+import { DateTime } from './modules/luxon.js';
+import Books from './modules/books.js';
+import UserI from './modules/form.js';
+import Library from './modules/library.js';
 
+const currentDate = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
+document.getElementById('present-time').innerHTML = currentDate;
+
+document.addEventListener('DOMContentLoaded', UserI.displayBooks);
+
+document.querySelector('#formID').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const title = document.querySelector('#book-title').value;
+  const author = document.querySelector('#book-author').value;
+  const book = new Books(title, author);
+  UserI.addBookToList(book);
+  Library.addBook(book);
+  UserI.clearFields();
+});
+
+document.querySelector('#book-list').addEventListener('click', (e) => {
+  UserI.deleteBook(e.target);
+  Library.removeBook(e.target.previousElementSibling.previousElementSibling.textContent);
+});
+const bookList = document.querySelector('.title');
+const listBtn = document.querySelector('#list');
+const formContainer = document.querySelector('#book-form');
+const contactInfo = document.querySelector('#contact-info');
+
+listBtn.addEventListener('click', () => {
+  bookList.classList.remove('hide');
+  formContainer.classList.add('hide');
+  contactInfo.classList.add('hide');
+});
+
+window.addEventListener('load', () => {
+  bookList.classList.remove('hide');
+  formContainer.classList.add('hide');
+  contactInfo.classList.add('hide');
+});
+
+const addNewBtn = document.querySelector('#new');
+
+addNewBtn.addEventListener('click', () => {
+  bookList.classList.add('hide');
+  formContainer.classList.remove('hide');
+  contactInfo.classList.add('hide');
+});
+
+const contactBtn = document.querySelector('#contact');
+contactBtn.addEventListener('click', () => {
+  bookList.classList.add('hide');
+  formContainer.classList.add('hide');
+  contactInfo.classList.remove('hide');
+});
---
+++ modules/library.js
@@ -0,0 +1,32 @@
+class Library {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Library.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(title) {
+    const books = Library.getBooks();
+
+    books.forEach((book, index) => {
+      if (book.title === title) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+export default Library;
